194aa11523f499748ff2b639a391fbc20778455c
JaneGoltong <nyamjenniferigbarak@gmail.com>
2025-04-15 11:05:36 +0100

    first commit

---
+++ objectMethod.js
@@ -0,0 +1,10 @@
+// Given two user profile objects (e.g., personalInfo and contactInfo), merge them into a single userProfile object.
+
+let personalInfo ={
+    name: "Jennifer",
+    gender:"Female",
+    age : 24  
+} 
+let contactInfo = {
+    email : "nyamjenniferigbarak@gmail.com"
+}
